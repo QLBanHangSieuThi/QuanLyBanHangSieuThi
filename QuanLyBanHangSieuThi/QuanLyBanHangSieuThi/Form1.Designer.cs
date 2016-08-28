74324d1bd48ac6b54793759dc8933bf68cd21d63
MinhHoa7605 <minhhoanguyen95@gmail.com>
2016-08-28 09:14:57 +0700

    hoa update

--- QuanLyBanHangSieuThi/QuanLyBanHangSieuThi/Form1.Designer.cs
+++ QuanLyBanHangSieuThi/QuanLyBanHangSieuThi/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -68,11 +69,20 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Quản lý dự án phần mềm";
             // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(71, 210);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(144, 39);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.comboBox1);
@@ -90,6 +100,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
