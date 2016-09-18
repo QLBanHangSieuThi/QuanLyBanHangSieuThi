96668e9774072e84cb4808e926691230a0cad29e
Tran Dac <trandc95@gmail.com>
2016-09-18 09:55:35 +0700

    cập nhật thư mục QL nhập xuất, frmNhapHangKhachTraLai

---
+++ QuanLyBanHangSieuThi/FrQLNhapHang/frmNhapHangKhachTra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHangSieuThi.FrQLNhapHang
+{
+    public partial class frmNhapHangKhachTra : DevComponents.DotNetBar.Office2007Form
+    {
+        public frmNhapHangKhachTra()
+        {
+            InitializeComponent();
+        }
+    }
+}
